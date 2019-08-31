bc25fa9aa0c1ee0a1f00f9bab86ff77f5a1d2f1c
EMPRRFMNKND <43456160+EMPRRFMNKND@users.noreply.github.com>
2019-08-31 12:16:07 -0300

    Atualização nas classes auxiliares do Executável (ClienteBo, TelefoneBO).

---
+++ src/main/java/model/bo/ClienteBO.java
@@ -0,0 +1,16 @@
+package model.bo;
+
+import model.entity.Cliente;
+
+public class ClienteBO {
+
+	public String excluir(Cliente cliente) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String salvar(Cliente novoCliente) {
+		return null;
+		
+	}
+}
